281ffae42ea247fda70536981231624be762051c
kramstyles <kramstyles@outlook.com>
2023-05-08 18:22:29 +0100

    added files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ColorList from "./components";
+
+function App() {
+  return (
+    <div className="container">
+      <ColorList/>
+    </div>
+  );
+}
+
+export default App;
